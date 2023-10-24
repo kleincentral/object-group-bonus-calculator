673f5e6232f2f01b928b9d7558224e2bd89549a6
Mark Klein <kleincentral@gmail.com>
2023-10-24 15:05:04 -0500

    Group project with Jacob

--- client.js
+++ client.js
@@ -29,10 +29,16 @@ const employees = [
     employeeNumber: '89068',
     annualSalary: '35000',
     reviewRating: 1
+  },
+  {
+    name: 'Mat',
+    employeeNumber: '2',
+    annualSalary: '100000',
+    reviewRating: 2
   }
 ];
 
-console.log('array of employee data: ',  employees );
+console.log('array of employee data:',  employees );
 
 
 // YOU SHOULD NOT NEED TO CHANGE ANYTHING ABOVE THIS POINT
@@ -43,15 +49,102 @@ console.log('array of employee data: ',  employees );
 // This is not a race. Everyone on your team should understand what is happening.
 // Ask questions when you don't.
 
+for (person of employees) {
 
-
-
-// This function will calculate 1 employee's bonus!
-//
-function calculateIndividualEmployeeBonus( employee ) {  
-  // your logic here
-  
-  
-  // return new object with bonus results
+  let employeeBonus = calculateIndividualEmployeeBonus(person);
+  console.log(employeeBonus);
+  // console.log(person);
 
 }
+
+// This function will calculate 1 employee's bonus!
+function calculateIndividualEmployeeBonus( employee ) {  
+  // your logic here
+  let updatedEmployeeInformation = {};
+  
+  let name = employee.name;
+  updatedEmployeeInformation['name'] = name;
+
+  let bonusPercentage = totalBonusPrecentage(employee)
+  updatedEmployeeInformation['bonusPercentage'] = bonusPercentage
+
+  let totalBonus = employee.annualSalary * bonusPercentage;
+  totalBonus = Math.round(totalBonus);
+  updatedEmployeeInformation[`totalBonus`] = totalBonus;
+
+  let actualSalary = employee.annualSalary/1;
+
+  let totalCompensation = totalBonus+actualSalary
+  updatedEmployeeInformation["totalCompensation"] = totalCompensation;
+
+  // console.log(updatedEmployeeInformation);  
+  // return new object with bonus results
+
+  return updatedEmployeeInformation;
+
+}
+
+function totalBonusPrecentage (employee) {
+
+  let bonus = 0
+  if (employee.reviewRating <= 2) {
+    console.log("Fuck you", employee.name);
+  }
+  else if (employee.reviewRating === 3 ) {
+    bonus = .04;
+  }
+  else if (employee.reviewRating === 4 ) {  
+    bonus = .06;
+  }
+  else if (employee.reviewRating === 5 ) {
+    bonus = .10;
+  }
+
+  if (employee.annualSalary > 65000) {
+    bonus -= .01;
+  }
+
+  if (employee.employeeNumber >= 1000) {
+    bonus += .05;
+  }
+
+  if (bonus > .13) {
+    bonus = .13;
+    console.log("Thank you for working hard", employee.name);
+  }
+  else if (bonus < 0) {
+    bonus = 0;
+    console.log("Fuck you in particular", employee.name);
+  }
+
+  // console.log(bonus, employee.name);
+  return bonus
+}
+
+
+
+
+/*
+
+The first thing we do is we look through each person in our employees list. 
+Once we are looing at our employee, and begin to calculate their bonus.
+
+We then set aside a list to collect our information in, and first place our employee's name inside.
+
+After this, we look at our employee's ratings to properly caclulate their bonus'
+
+Many factors go into this decision, such as their review rating, how long they have been at the company, and how much they are making.
+Just to ensure our employees are being paid properly.
+
+We then save their bonus precentage which was calculated from those factors and added it to our list.
+
+We then calculated their total bonus using the money they earned multiplied by the percentage and added it to our list.
+
+Then we added their bonus to their current salary to determine how much they are paid. Which we added to our list.
+
+Then once had collected this list we saved it, and moved to the next employee doing the same for them
+
+
+
+*/
+
